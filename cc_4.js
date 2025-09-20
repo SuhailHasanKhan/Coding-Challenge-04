9d552b164e201198971c763d5745601295f97268
Suhail Hasan Khan <shasankhan@usf.edu>
2025-09-20 14:45:55 -0400

    created products array

--- cc_4.js
+++ cc_4.js
@@ -1,3 +1,11 @@
 // Coding Challenge 00
 
 // Code goes here
+
+let products = [
+    {sku: '001', name: 'Headphones', category: 'electronics', price: 79.99, inventory: 56},
+    {sku: '002', name: 'Running Shorts', category: 'apparel', price: 29.99, inventory: 77},
+    {sku: '003', name: 'Ranch', category: 'groceries', price: 9.99, inventory: 102},
+    {sku: '004', name: 'Toilet Paper', category: 'groceries', price: 4.99, inventory: 343},
+    {sku: '005', name: 'Drill Bits', category: 'default', price: 49.99, inventory: 96},
+]

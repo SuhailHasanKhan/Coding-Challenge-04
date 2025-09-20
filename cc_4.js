d59a9164c4b1f1a5ecc71451062792d816ec8cb3
Suhail Hasan Khan <shasankhan@usf.edu>
2025-09-20 14:54:13 -0400

    created a for of loop for discounts

--- cc_4.js
+++ cc_4.js
@@ -8,4 +8,27 @@ let products = [
     {sku: '003', name: 'Ranch', category: 'groceries', price: 9.99, inventory: 102},
     {sku: '004', name: 'Toilet Paper', category: 'groceries', price: 4.99, inventory: 343},
     {sku: '005', name: 'Drill Bits', category: 'default', price: 49.99, inventory: 96},
-]
+];
+
+for (let product of products) {
+    let discount = 0
+    switch(product.category) {
+        case "electronics":
+            discount = 0.2
+            break;
+        case "apparel":
+            discount = 0.15
+            break;
+        case "groceries":
+        case "household":
+            discount = 0.1
+            break;
+        case "default":
+            discount = 0
+            break;
+    }
+    let promoPrice = product.price*(1-discount);
+    product.promoPrice = promoPrice;
+};
+
+console.log(products);
